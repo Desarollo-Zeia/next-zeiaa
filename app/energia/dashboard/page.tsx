c5babaf8b704289746695015ac55ed200c5272bf
Alonso <morantealo@hotmail.com>
2025-03-10 14:21:25 -0500

    new

--- app/energia/dashboard/page.tsx
+++ app/energia/dashboard/page.tsx
@@ -1,48 +1,30 @@
-import { consume, consumeGraph } from "@/app/sevices/energy/data";
-import FiltersContainer from "@/app/ui/filters/filters-container";
-import { getCompanyData } from "@/app/lib/auth";
-import { getEnergyCompanyDetails } from "@/app/sevices/energy/enterprise/data";
-import HeadquarterEnergyFilter from "@/app/ui/energia/filters/headquarter-energy-filter";
-import PanelsFilterEnergy from "@/app/ui/energia/filters/panels-energy-filter";
-import { DateRangePicker } from "@/app/ui/energia/filters/datepicker-energy-filter";
-import MeasurementTable from "@/app/ui/energia/consumo/measurement-table";
-import MeasurementGraph from "@/app/ui/energia/consumo/measurement-graph";
+import { consume, consumeGraph } from "@/app/sevices/energy/data"
+import FiltersContainer from "@/app/ui/filters/filters-container"
+import { getCompanyData } from "@/app/lib/auth"
+import { getEnergyCompanyDetails } from "@/app/sevices/energy/enterprise/data"
+import HeadquarterEnergyFilter from "@/app/ui/energia/filters/headquarter-energy-filter"
+import PanelsFilterEnergy from "@/app/ui/energia/filters/panels-energy-filter"
+import { DateRangePicker } from "@/app/ui/energia/filters/datepicker-energy-filter"
+import MeasurementTable from "@/app/ui/energia/consumo/measurement-table"
+import MeasurementGraph from "@/app/ui/energia/consumo/measurement-graph"
+import { SearchParams } from "@/app/type"
 
-interface PageProps {
-  searchParams: {
-    headquarter?: string;
-    panel?: string;
-    date_after?: string;
-    date_before?: string;
-    unit?: string;
-    indicator?: string;
-    page?: string;
-    last_by?: string;
-  };
-}
 
-export default async function Page({ searchParams }: PageProps) {
-  const { companies } = await getCompanyData();
 
-  const {
-    headquarter = "1",
-    panel = "1",
-    date_after,
-    date_before,
-    unit = "V",
-    indicator = "P",
-    page = "1",
-    last_by = "day"
-  } = searchParams;
+export default async function Page({ searchParams }: SearchParams) {
+  const { companies } = await getCompanyData()
+
+  const { headquarter = '1', panel = '1', date_after, date_before, unit = 'V', indicator = 'P', page = '1', last_by = 'day' } = await searchParams
 
   const readings = await consume({
     date_after,
     date_before,
     headquarterId: headquarter,
     panelId: panel,
-    unit,
+    unit: unit,
     page
-  });
+    
+  })
 
   const readingsGraph = await consumeGraph({
     date_after,
@@ -52,32 +34,22 @@ export default async function Page({ searchParams }: PageProps) {
     indicador: indicator,
     unit,
     last_by
-  });
+  })
 
-  const energyDetails = await getEnergyCompanyDetails({
-    headquarterId: companies[0].id
-  });
+  const energyDetails = await getEnergyCompanyDetails({ headquarterId: companies[0].id })
 
   return (
     <div className="w-full">
       <FiltersContainer>
-        <HeadquarterEnergyFilter
-          energyHeadquarter={energyDetails.energy_headquarters}
-        />
-        <PanelsFilterEnergy
-          energyPanels={energyDetails.energy_headquarters[0].electrical_panels}
-        />
-        <DateRangePicker />
+        <HeadquarterEnergyFilter energyHeadquarter={energyDetails.energy_headquarters} />
+        <PanelsFilterEnergy energyPanels={energyDetails.energy_headquarters[0].electrical_panels} />
+        <DateRangePicker/>
       </FiltersContainer>
       <div className="flex">
-        <MeasurementTable readings={readings} unit={unit} />
-        <MeasurementGraph
-          data={readingsGraph}
-          unit={unit}
-          count={readings.count}
-          frequency={last_by}
-        />
+        <MeasurementTable readings={readings} unit={unit}/>
+        <MeasurementGraph data={readingsGraph} unit={unit} count={readings.count} frequency={last_by}/>
       </div>
     </div>
-  );
+  )
 }
+
