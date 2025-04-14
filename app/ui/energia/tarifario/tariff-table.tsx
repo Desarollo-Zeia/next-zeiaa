8c27f3d10318fc41ada4d0b527939f28c234266f
Alonso Worker <desarollo@zeia.com.pe>
2025-04-14 10:42:03 -0500

    tariff component

--- app/ui/energia/tarifario/tariff-table.tsx
+++ app/ui/energia/tarifario/tariff-table.tsx
@@ -47,24 +47,24 @@ export default function TariffTable() {
   const tariffRating = billing_data.tariff_rating
 
   return (
-    <Card className="w-full mx-auto overflow-hidden border-none shadow-lg">
+    <Card className="w-full mx-auto overflow-hidden border-none shadow-lg relative">
       <CardHeader className="bg-[#01b7ca] py-6">
         <CardTitle className="text-white text-xl font-bold tracking-tight">
           BAJA TENSIÓN : TARIFA BT4
         </CardTitle>
       </CardHeader>
-      <CardContent className="bg-[#f4f9ff] p-0 hover:bg-[#01b7ca]">
+      <CardContent className="bg-[#f4f9ff] p-0">
         <Table className="w-full">
           <TableHeader>
-            <TableRow className="bg-[#01b7ca]">
-              <TableHead className="text-white font-medium">Tarifa con doble medición de energía activa y contratación o medición de una potencia 2E1P</TableHead>
+            <TableRow className="bg-[#01b7ca] hover:bg-[#01b7ca]">
+              <TableHead className="text-white font-medium ">Tarifa con doble medición de energía activa y contratación o medición de una potencia 2E1P</TableHead>
               <TableHead className="text-center text-white font-medium">Unidad</TableHead>
               <TableHead className="text-center text-white font-medium">Tarifa</TableHead>
               <TableHead className="text-center text-white font-medium">Consumo</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
+            <TableRow className="hover:bg-white">
               <TableCell>
                  Cargo fijo mensual
               </TableCell>
@@ -113,14 +113,52 @@ export default function TariffTable() {
               <TableCell className="text-center">
                  ctm. S//kWh
               </TableCell>
-              <div>
-                <TableCell className="text-center">
-                  {tariffData.billing_data.tariff_rating ? tariffData.billing_data.cargo_por_potencia_activa_de_generacion_para_usuarios.presentes_en_punta : tariffData.billing_data.cargo_por_potencia_activa_de_generacion_para_usuarios.presentes_fuera_de_punta}
-                </TableCell>
-              </div>
+              <TableCell className="text-center">
+                {tariffData.billing_data.tariff_rating ? tariffData.billing_data.cargo_por_potencia_activa_de_generacion_para_usuarios.presentes_en_punta : tariffData.billing_data.cargo_por_potencia_activa_de_generacion_para_usuarios.presentes_fuera_de_punta}
+              </TableCell>
               <TableCell className="text-center">
                   {tariffData.importe.cargo_por_potencia_activa_generacion}
-                </TableCell>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Cargo por potencia activa de redes de distribución para usuarios
+              </TableCell>
+              <TableCell className="text-center">
+                 ctm. S//kWh
+              </TableCell>
+              <TableCell className="text-center">
+                {tariffData.billing_data.tariff_rating ? tariffData.billing_data.cargo_por_potencia_activa_de_redes_de_distribucion_para_usuarios.presentes_en_punta : tariffData.billing_data.cargo_por_potencia_activa_de_redes_de_distribucion_para_usuarios.presentes_fuera_de_punta}
+              </TableCell>
+              <TableCell className="text-center">
+                  {tariffData.importe.cargo_por_potencia_activa_distribucion}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                Cargo por energía reactiva que exceda el 30% del total de la energía activa
+              </TableCell>
+              <TableCell className="text-center">
+                ctm S//kVar.h
+              </TableCell>
+              <TableCell className="text-center">
+                {tariffData.billing_data["cargo_por_energia_reactiva_que_exceda_el_30%_del_total_de_energia_activa"]}
+              </TableCell>
+              <TableCell className="text-center">
+                  {tariffData.importe.cargo_por_energia_reactiva}
+              </TableCell>
+            </TableRow>
+            <TableRow className="bg-[#01b7ca] hover:bg-[#01b7ca]">
+              <TableCell>
+              </TableCell>
+              <TableCell className="text-center">
+              </TableCell>
+              <TableCell className="text-center text-white">
+                  Costo total
+              </TableCell>
+              <TableCell className="text-center text-white font-bold">
+                  {tariffData.importe.total}
+              </TableCell>
             </TableRow>
           </TableBody>
         </Table>
