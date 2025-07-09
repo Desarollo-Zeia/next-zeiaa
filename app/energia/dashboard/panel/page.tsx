b5a7794194e3b8a5a0050fdaa13da9911f03646c
Alonso Worker <desarollo@zeia.com.pe>
2025-07-09 17:01:25 -0500

    fiz

--- app/energia/dashboard/panel/page.tsx
+++ app/energia/dashboard/panel/page.tsx
@@ -1,12 +1,15 @@
+import { getHeadquarters } from '@/app/sevices/filters/data'
 import { dashboardTable } from '@/app/sevices/panel/data'
 import { SearchParams } from '@/app/type'
+import HeadquarterEnergyFilter from '@/app/ui/energia/filters/headquarter-energy-filter'
 import ChartComponent from '@/app/ui/energia/panel/chart'
 import TableComponent from '@/app/ui/energia/panel/table'
+import FiltersContainer from '@/app/ui/filters/filters-container'
 import React from 'react'
 
 export default async function page({ searchParams }: SearchParams) {
 
-  const { headquarter = '1' , panel = '1',  date_after = new Date(), date_before = new Date()} = await searchParams
+  const { headquarter, panel = '1',  date_after = new Date(), date_before = new Date()} = await searchParams
 
   console.log({
     panel,
@@ -14,10 +17,18 @@ export default async function page({ searchParams }: SearchParams) {
     date_before
   })
 
-  const dashboardTableReadings = await dashboardTable({ headquarterId: headquarter })
+  const headquarters  = await getHeadquarters()
+  const { results } = headquarters
+  const firstHeadquarter = headquarter || results[0].id.toString()
+
+  const dashboardTableReadings = await dashboardTable({ headquarterId: firstHeadquarter })
 
   return (
     <div className="relative p-6 flex justify-center items-center gap-8">
+      <FiltersContainer>
+        <HeadquarterEnergyFilter energyHeadquarter={headquarters.results} energy={firstHeadquarter}/>
+        {/* <PanelsFilterEnergy energyPanels={energyDetails.energy_headquarters?.[0].electrical_panels} /> */}
+      </FiltersContainer>
       <ChartComponent/>
       <TableComponent readings={dashboardTableReadings}/> 
     </div>
