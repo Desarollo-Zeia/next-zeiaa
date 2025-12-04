8c8a115154d8e361a14f0965e1c0c7a8c90fe8eb
Alonso <morantealo@hotmail.com>
2025-12-04 16:39:58 -0500

    a

--- app/lib/cache.ts
+++ app/lib/cache.ts
@@ -1,4 +1,4 @@
-import { unstable_cache, revalidateTag } from 'next/cache'
+import { unstable_cache } from 'next/cache'
 
 // Cache durations in seconds
 export const CACHE_DURATION = {
@@ -48,60 +48,3 @@ export function generateCacheKey(prefix: string, params: Record<string, unknown>
   return `${prefix}-${sortedParams}`
 }
 
-// Cache invalidation functions
-export async function invalidateEnergyCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.ENERGY)
-}
-
-export async function invalidateAmbientalCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.AMBIENTAL)
-}
-
-export async function invalidateOcupacionalCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.OCUPACIONAL)
-}
-
-export async function invalidateEnterpriseCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.ENTERPRISE)
-}
-
-export async function invalidateFiltersCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.FILTERS)
-}
-
-export async function invalidateAllCache() {
-  'use server'
-  await Promise.all([
-    invalidateEnergyCache(),
-    invalidateAmbientalCache(),
-    invalidateOcupacionalCache(),
-    invalidateEnterpriseCache(),
-    invalidateFiltersCache(),
-  ])
-}
-
-// Specific invalidation functions for granular control
-export async function invalidateReadingsCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.READINGS)
-}
-
-export async function invalidateAlertsCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.ALERTS)
-}
-
-export async function invalidatePanelsCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.PANELS)
-}
-
-export async function invalidateHeadquartersCache() {
-  'use server'
-  revalidateTag(CACHE_TAGS.HEADQUARTERS)
-}
