4094eb5a026626b492b4203507e4d262cf701a68
Alonso <morantealo@hotmail.com>
2025-12-11 09:30:20 -0500

    search Params apperently is not admited at Page

--- app/ocupacional/dashboard/layout.tsx
+++ app/ocupacional/dashboard/layout.tsx
@@ -4,8 +4,7 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
-// import { fetchWithAuth } from "@/app/lib/api"
-// import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -13,37 +12,35 @@ interface LayoutProps {
 
 export default async function Layout({ children }: LayoutProps) {
 
-  // const userData = await fetchWithAuth('/enterprise/api/enterprise/detail/')
-
-  // const ocupacional = {
-  //   rooms: '/ocupacional/dashboard/rooms',
-  //   monitoreo: '/ocupacional/dashboard/monitoreo',
-  //   analisis: {
-  //     title: 'Análisis',
-  //     isActive: true,
-  //     url: '/ocupacional/dashboard/analisis',
-  //     items: [
-  //       {
-  //         title: 'Indicadores',
-  //         url: '/ocupacional/dashboard/analisis/indicadores'
-  //       },
-  //       {
-  //         title: 'Picos Históricos',
-  //         url: '/ocupacional/dashboard/analisis/picoshistoricos'
-  //       },
-  //       {
-  //         title: 'Estadísticas',
-  //         url: '/ocupacional/dashboard/analisis/estadisticas'
-  //       }
-  //     ]
-  //   },
-  //   // covid: '/ocupacional/dashboard/covid',
-  //   alertas: '/ocupacional/dashboard/alertas'
-  // }
+  const ocupacional = {
+    rooms: '/ocupacional/dashboard/rooms',
+    monitoreo: '/ocupacional/dashboard/monitoreo',
+    analisis: {
+      title: 'Análisis',
+      isActive: true,
+      url: '/ocupacional/dashboard/analisis',
+      items: [
+        {
+          title: 'Indicadores',
+          url: '/ocupacional/dashboard/analisis/indicadores'
+        },
+        {
+          title: 'Picos Históricos',
+          url: '/ocupacional/dashboard/analisis/picoshistoricos'
+        },
+        {
+          title: 'Estadísticas',
+          url: '/ocupacional/dashboard/analisis/estadisticas'
+        }
+      ]
+    },
+    // covid: '/ocupacional/dashboard/covid',
+    alertas: '/ocupacional/dashboard/alertas'
+  }
 
   return (
     <SidebarProvider >
-      {/* <AppSidebar userData={userData} module={ocupacional} /> */}
+      <AppSidebar module={ocupacional} />
       <SidebarInset>
         <header className="flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
