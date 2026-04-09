1b1578e1203b4379a002060024dad65c61e6dbde
Alonso <morantealo@hotmail.com>
2026-04-09 11:50:08 -0500

    amplying timeout

--- app/energia/dashboard/tarifario/components/tariff-data.tsx
+++ app/energia/dashboard/tarifario/components/tariff-data.tsx
@@ -13,7 +13,7 @@ import NoResultsFound from "@/app/ui/no-result"
 import { getToken } from "@/app/lib/auth"
 import { formatNumberWithCommas } from "@/app/utils/func"
 
-export const maxDuration = 60
+export const maxDuration = 300
 
 interface TariffDataProps {
   headquarterId: string
