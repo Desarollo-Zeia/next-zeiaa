fedae7a7cecd8eb30ebfd5936d91660bf4346b6f
Zeia <desarollo@zeia.com.pe>
2026-05-06 15:07:36 -0500

    style: make headquarter filter text semibold

--- app/ui/energia/filters/headquarter-energy-filter.tsx
+++ app/ui/energia/filters/headquarter-energy-filter.tsx
@@ -59,7 +59,7 @@ export default function HeadquarterEnergyFilter({ energyHeadquarter = [], energy
   return (
     <div>
       <Select value={currentHeadquarterId || energy} onValueChange={handleHeadquarterChange}>
-        <SelectTrigger className="w-[240px] border-[#707070] text-[#707070] bg-white">
+        <SelectTrigger className="w-[240px] border-[#707070] text-[#707070] bg-white font-semibold">
           <SelectValue placeholder="Seleccionar sede" />
         </SelectTrigger>
         <SelectContent>
